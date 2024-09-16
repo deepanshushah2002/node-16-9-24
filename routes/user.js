ba761520d4e488fe2025352755c2e4517c19b73e
deepanshushah2002 <deepanshusha9@gmail.com>
2024-09-16 12:47:36 +0530

    updation

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.get('/users',(req,res)=>{
     usercontroller.getUser(req,res);
 })
-
+router.get('/user/:id',(req,res)=>{
+   usercontroller.getParticularUser(req,res) 
+})
 module.exports = router;
